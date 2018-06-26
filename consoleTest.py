cccda248da61c64d1decce70d5354214787be5ab
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-06-26 22:41:51 +0200

    added adapter template

--- consoleTest.py
+++ consoleTest.py
@@ -14,36 +14,37 @@ from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.backends import default_backend
 from own_turn_on import turn_on
 
-"""Configuration"""
-testCurve = ec.SECP256R1()
-xboxKey = ec.EllipticCurvePublicNumbers(curve=testCurve, 
-x=29205441515073923487559868131118644438058724507779850563188099066744850429792, 
-y=65978933634580207595475043557351594754297380965211948229701265706146817682936)
-
-#print(xboxKey.public_key(default_backend()))
-
-xboxOne = Console(address='192.168.178.78', name='XboxOneX', 
-                  uuid=UUID('571a48af-6109-157f-1b3f-dd189ccab114'), 
-                  liveid='FD003F65D1219026', 
-                  flags=PrimaryDeviceFlag.AllowAnonymousUsers, 
-                  public_key=xboxKey.public_key(default_backend()))
-
-xboxOne.add_manager(InputManager) # add Input Manager for Gamepad Actions
-
-"""Start Xbox and Connection"""
-turn_on(ip_addr=xboxOne.address, live_id=xboxOne.liveid)
-
-print('connected before: ', xboxOne.connected)
-xboxOne.connect()
-print('connected after: ', xboxOne.connected)
-
-# wait after connection is established
-xboxOne.wait(1)
-
-"""Custom Commands"""
-#xboxOne.power_off()
-
-xboxOne.gamepad_input(GamePadButton.Menu)
-xboxOne.wait(4)
-xboxOne.gamepad_input(GamePadButton.DPadDown)
-#xboxOne.power_on(tries=5)
+def program():
+    """Configuration"""
+    testCurve = ec.SECP256R1()
+    xboxKey = ec.EllipticCurvePublicNumbers(curve=testCurve, 
+    x=29205441515073923487559868131118644438058724507779850563188099066744850429792, 
+    y=65978933634580207595475043557351594754297380965211948229701265706146817682936)
+    
+    #print(xboxKey.public_key(default_backend()))
+    
+    xboxOne = Console(address='192.168.178.78', name='XboxOneX', 
+                      uuid=UUID('571a48af-6109-157f-1b3f-dd189ccab114'), 
+                      liveid='FD003F65D1219026', 
+                      flags=PrimaryDeviceFlag.AllowAnonymousUsers, 
+                      public_key=xboxKey.public_key(default_backend()))
+    
+    xboxOne.add_manager(InputManager) # add Input Manager for Gamepad Actions
+    
+    """Start Xbox and Connection"""
+    turn_on(ip_addr=xboxOne.address, live_id=xboxOne.liveid)
+    
+    print('connected before: ', xboxOne.connected)
+    xboxOne.connect()
+    print('connected after: ', xboxOne.connected)
+    
+    # wait after connection is established
+    xboxOne.wait(1)
+    
+    """Custom Commands"""
+    #xboxOne.power_off()
+    
+    xboxOne.gamepad_input(GamePadButton.Menu)
+    xboxOne.wait(4)
+    xboxOne.gamepad_input(GamePadButton.DPadDown)
+    #xboxOne.power_on(tries=5)
