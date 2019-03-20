6771574c4e63014f5c887d04c0fc0eb50796be50
Moritz Heusinger <moritz.heusinger@gmail.com>
2019-03-20 16:11:57 +0100

    adjust tests

--- admin/words.js
+++ admin/words.js
@@ -3,10 +3,10 @@
 'use strict';
 
 systemDictionary = {
-    "Your consoles ip address or hostname": {"en": "Your consoles ip address or hostname", "de": "IP-Adresse oder Hostname der Xbox"},
-    "Your consoles live id": {"en": "Your consoles live id", "de": "Live ID deiner Xbox"},
-    "Authenticate on Xbox Live": {"en": "Authenticate on Xbox Live", "de": "Authentifizierung bei Xbox Live"},
-    "E-mail address": {"en": "E-mail address", "de": "E-Mail Adresse"},
-    "Password": {"en": "Password", "de": "Passwort"},
-    "Please provide your Xbox Live account information": {"en": "Please enter your Xbox Live account information", "de": "Bitte Xbox Live Account Daten eingeben"}
+    'Your consoles ip address or hostname': {'en': 'Your consoles ip address or hostname', 'de': 'IP-Adresse oder Hostname der Xbox'},
+    'Your consoles live id': {'en': 'Your consoles live id', 'de': 'Live ID deiner Xbox'},
+    'Authenticate on Xbox Live': {'en': 'Authenticate on Xbox Live', 'de': 'Authentifizierung bei Xbox Live'},
+    'E-mail address': {'en': 'E-mail address', 'de': 'E-Mail Adresse'},
+    'Password': {'en': 'Password', 'de': 'Passwort'},
+    'Please provide your Xbox Live account information': {'en': 'Please enter your Xbox Live account information', 'de': 'Bitte Xbox Live Account Daten eingeben'}
 };
